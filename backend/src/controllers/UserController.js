1fc9d5fe4d5714f5a6248c8b1251a079b135675f
Anderson Santos <anderson.santos@eunerd.com.br>
2021-12-28 17:06:29 -0300

    realizado ajuste em routes e no UserController

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,10 +1,26 @@
 const User = require("../models/User")
 
 module.exports = {
+    async index(req, res){
+        const result = await User.findAll();
+        return res.json(result);
+    },
     async store(req, res){
         const { name, gender, age, hobby} = req.body
 
-        const user = await User.create({ name, gender, age, hobby});
-        return res.json(user);
+        const create_user = await User.create({ name, gender, age, hobby});
+        return res.json(create_user);
+    },
+    async update(req, res){
+        const { id } = req.params;
+        const { name, gender, age, hobby } = req.body;
+        const result = await User.upsert({name: name, gender: gender, age: age, hobby: hobby}, {where: {id: id}});
+        return res.json(result);
+    },
+
+    async delete(req, res){
+        const { id } = req.params;
+        await User.destroy({where:{id: id}})
+        return res.send();
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,37 +1,14 @@
 const express = require('express');
 const UserController =  require('./controllers/UserController');
 
-
 const routes = express.Router();
 
-const devs = [];
-
-routes.get('/dev', (request, response)=>{
-    
-    return response.json(devs);
-});
+routes.get('/dev', (UserController.index));
 
 routes.post('/dev', (UserController.store));
 
-routes.put('/dev/:id', (request, response)=>{
+routes.put('/dev/:id', (UserController.update));
 
-    const {id} = request.params;
-    const {name, ocupation} = request.body;
-    
-    const position = devs.findIndex(item => item.id == id);
-
-    const dev = { id,name, ocupation};
-
-    devs[position] = dev;
-    return response.json(dev);
-});
-
-routes.delete('/dev/:id', (request, response)=>{
-    const { id } = request.params;
-
-    const position = devs.findIndex(item => item.id === id);
-    devs.splice(position, 1);
-    return response.status(204).send()
-});
+routes.delete('/dev/:id', UserController.delete);
 
 module.exports = routes;
