64a79c1eac9e04a4c845fc79f09e1c386b1696c8
Anderson Santos <anderson.santos@eunerd.com.br>
2021-12-29 02:16:59 -0300

    Ajuste de status no CRUD

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -3,24 +3,23 @@ const User = require("../models/User")
 module.exports = {
     async index(req, res){
         const result = await User.findAll();
-        return res.json(result);
+        return res.status(200).json(result);
     },
     async store(req, res){
         const { name, gender, age, hobby} = req.body
-
         const create_user = await User.create({ name, gender, age, hobby});
-        return res.json(create_user);
+        return res.status(201).json(create_user);
     },
     async update(req, res){
         const { id } = req.params;
         const { name, gender, age, hobby } = req.body;
         const result = await User.upsert({name: name, gender: gender, age: age, hobby: hobby}, {where: {id: id}});
-        return res.json(result);
+        return res.status(200).json(result);
     },
 
     async delete(req, res){
         const { id } = req.params;
         await User.destroy({where:{id: id}})
-        return res.send();
+        return res.status(204).send();
     }
 }
