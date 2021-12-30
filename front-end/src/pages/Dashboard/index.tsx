395382b7a380512b7dc1f73f6b967b7fad2f7800
Anderson Santos <anderson.santos@eunerd.com.br>
2021-12-30 03:19:25 -0300

    Concluindo listagem dos Devs

--- front-end/src/pages/Dashboard/index.tsx
+++ front-end/src/pages/Dashboard/index.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Title, Form, ListDevs } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>
+    return(
+        <>
+            <Title>Cadastros de Devs</Title>
+
+            <Form>
+                <input placeholder="Pesquise Por Devs" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <ListDevs>
+                <a href="">
+                    <img src="https://avatars.githubusercontent.com/u/47918900?v=4" alt="Imagem de Perfil" />
+                    <div>
+                        <strong>Anderson Santos</strong>
+                        <p>Programador para o Mundo ser Melhor</p>
+                        <p>Nivel: Junior</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="">
+                    <img src="https://avatars.githubusercontent.com/u/47918900?v=4" alt="Imagem de Perfil" />
+                    <div>
+                        <strong>Anderson Santos</strong>
+                        <p>Programador para o Mundo ser Melhor</p>
+                        <p>Nivel: Junior</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="">
+                    <img src="https://avatars.githubusercontent.com/u/47918900?v=4" alt="Imagem de Perfil" />
+                    <div>
+                        <strong>Anderson Santos</strong>
+                        <p>Programador para o Mundo ser Melhor</p>
+                        <p>Nivel: Junior</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </ListDevs>
+        </>
+    );
 }
 
 export default Dashboard;
